9e5da6dc37ab7021eb1d1e993aada4949759a821
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2023-10-14 12:00:58 -0300

    Update Match.cpp

--- source/plugins/components/Match.cpp
+++ source/plugins/components/Match.cpp
@@ -15,6 +15,7 @@
 
 #include "../../kernel/simulator/Model.h"
 #include "../../kernel/simulator/Simulator.h"
+#include "../../kernel/simulator/SimulationControlAndResponse.h"
 
 #ifdef PLUGINCONNECT_DYNAMIC
 
@@ -28,6 +29,28 @@ ModelDataDefinition* Match::NewInstance(Model* model, std::string name) {
 }
 
 Match::Match(Model* model, std::string name) : ModelComponent(model, Util::TypeOf<Match>(), name) {
+	// SimulationControlString* propRule = new SimulationControlString(
+	// 								std::bind(&Match::MatchExpression, this), std::bind(&Match::setMatchExpression, this, std::placeholders::_1, Util::TimeUnit::unknown),
+	// 								Util::TypeOf<Match>(), getName(), "MatchExpression", "");
+	SimulationControlUInt* propNumberQueues = new SimulationControlUInt(
+									std::bind(&Match::getNumberOfQueues, this), std::bind(&Match::setNumberOfQueues, this, std::placeholders::_1),
+									Util::TypeOf<Match>(), getName(), "NumberOfQueues", "");
+	SimulationControlString* propMatchSize = new SimulationControlString(
+									std::bind(&Match::getMatchSize, this), std::bind(&Match::setMatchSize, this, std::placeholders::_1),
+									Util::TypeOf<Match>(), getName(), "MatchSize", "");
+	SimulationControlString* propAttributeName = new SimulationControlString(
+									std::bind(&Match::getAttributeName, this), std::bind(&Match::setAttributeName, this, std::placeholders::_1),
+									Util::TypeOf<Match>(), getName(), "AttributeName", "");
+
+	// _parentModel->getControls()->insert();
+	_parentModel->getControls()->insert(propNumberQueues);
+	_parentModel->getControls()->insert(propMatchSize);
+	_parentModel->getControls()->insert(propAttributeName);
+
+	// _addProperty();
+	_addProperty(propNumberQueues);
+	_addProperty(propMatchSize);
+	_addProperty(propAttributeName);
 }
 
 std::string Match::show() {
@@ -170,4 +193,3 @@ PluginInformation * Match::GetPluginInformation() {
 	// ...
 	return info;
 }
-
