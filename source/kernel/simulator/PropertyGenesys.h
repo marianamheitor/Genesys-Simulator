fe44ca82dd2a9c18e01d5f802fa2cb9274876e59
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2023-10-14 11:55:47 -0300

    Update PropertyGenesys.h

--- source/kernel/simulator/PropertyGenesys.h
+++ source/kernel/simulator/PropertyGenesys.h
@@ -1,4 +1,20 @@
 #ifndef PROPERTY_H
 #define PROPERTY_H
 
+#include "SimulationControlAndResponse.h"
+#include "ModelComponent.h"
+
+class PropertyEditorGenesys {
+public: // constructor and destructor
+    PropertyEditorGenesys() = default;
+    virtual ~PropertyEditorGenesys() = default;
+public: // get
+    std::list<ModelComponent*> getElements();
+public:
+    void changeProperty(std::string id, std::string attribute, std::string value);
+    void addElement(ModelComponent* component);
+private: // attributes
+    std::list<ModelComponent*> _elements;
+};
+
 #endif
