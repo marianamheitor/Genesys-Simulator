41e285fad6176ffbc3d5e9629c5208caf5150cbf
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2023-10-22 04:21:31 -0300

    Update Leave.cpp

--- source/plugins/components/Leave.cpp
+++ source/plugins/components/Leave.cpp
@@ -27,6 +27,14 @@ ModelDataDefinition* Leave::NewInstance(Model* model, std::string name) {
 }
 
 Leave::Leave(Model* model, std::string name) : ModelComponent(model, Util::TypeOf<Leave>(), name) {
+	// SimulationControlGeneric<Station*>* propStation = new SimulationControlGeneric<Station*>(
+	// 								std::bind(&Leave::getStation, this), std::bind(&Leave::setStation, this, std::placeholders::_1),
+	// 								Util::TypeOf<Leave>(), getName(), "Station", "");
+
+	// _parentModel->getControls()->insert(propStation);
+
+	// setting properties
+	// _addProperty(propStation);
 }
 
 std::string Leave::show() {
