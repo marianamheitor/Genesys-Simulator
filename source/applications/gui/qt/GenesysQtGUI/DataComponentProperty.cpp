d95eed5bb8e00195bf3636bdf4b0b2cc4e239489
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2023-11-16 20:32:37 -0300

    Update DataComponentProperty.cpp

--- source/applications/gui/qt/GenesysQtGUI/DataComponentProperty.cpp
+++ source/applications/gui/qt/GenesysQtGUI/DataComponentProperty.cpp
@@ -2,12 +2,19 @@
 
 DataComponentProperty::DataComponentProperty() {
     _window = new QWidget;
+    _view = new QTreeWidget(_window);
 };
 
 DataComponentProperty::~DataComponentProperty() {
     delete _window;
+    delete _view;
 };
 
-void DataComponentProperty::open_window() {
+void DataComponentProperty::open_window(QString newValue) {
+    // add element
+    QTreeWidgetItem* newItem = new QTreeWidgetItem(_view);
+    _view->addTopLevelItem(newItem);
+    newItem->setText(0,newValue);
+    
     _window->show();
 }
