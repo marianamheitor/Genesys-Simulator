467bd97b7000c23aca0b26c2ac9784a83585041d
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2023-11-19 18:43:01 -0300

    Update DataComponentProperty.h

--- source/applications/gui/qt/GenesysQtGUI/DataComponentProperty.h
+++ source/applications/gui/qt/GenesysQtGUI/DataComponentProperty.h
@@ -1,22 +1,35 @@
 #ifndef DATACOMPONENTPROPERTY_H
 #define DATACOMPONENTPROPERTY_H
 
+#include <iostream>
+
 #include <QWidget>
 #include <QTreeWidget>
 #include <QTreeWidgetItem>
+#include <QPushButton>
+#include <QInputDialog>
+
+#include "../../../../kernel/simulator/PropertyGenesys.h"
 
 
-class DataComponentProperty {
+class DataComponentProperty : public QObject {
 public:
-    DataComponentProperty();
+    DataComponentProperty(PropertyEditorGenesys* editor, SimulationControl* property);
     ~DataComponentProperty();
 
 public:
-    void open_window(QString newValue);
+    void open_window();
+
+private Q_SLOTS:
+    void addElement(PropertyEditorGenesys* editor, SimulationControl* property);
+    void removeElement(PropertyEditorGenesys* editor, SimulationControl* property);
 
 private:
     QWidget* _window;
     QTreeWidget* _view;
+    QPushButton* _add;
+    QPushButton* _remove;
+    QInputDialog* _confirmation;
 };
 
 #endif // DATACOMPONENTPROPERTY_H
