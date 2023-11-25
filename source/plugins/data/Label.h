d7dc7477a457619a35c1fb66bfa081692ebff067
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2023-11-25 13:01:20 -0300

    Update Label.h

--- source/plugins/data/Label.h
+++ source/plugins/data/Label.h
@@ -1,132 +1,46 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.cc to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.h to edit this template
  */
 
-/*
- * File:   Label.cpp
+/* 
+ * File:   Label.h
  * Author: rlcancian
  *
  * Created on 15 de janeiro de 2022, 10:13
  */
 
-#include "Label.h"
-#include "../../kernel/simulator/Model.h"
+#ifndef LABEL_H
+#define LABEL_H
+#include "../../kernel/simulator/ModelDataDefinition.h"
+#include "../../kernel/simulator/Entity.h"
 
-#ifdef PLUGINCONNECT_DYNAMIC
+class Label : public ModelDataDefinition {
+public:
+	Label(Model* model, std::string name = "");
+	virtual ~Label() = default;
+public: // static
+	static ModelDataDefinition* LoadInstance(Model* model, PersistenceRecord *fields);
+	static PluginInformation* GetPluginInformation();
+	static ModelDataDefinition* NewInstance(Model* model, std::string name = "");
+public:
+	virtual std::string show();
+	void setLabel(std::string _label);
+	std::string getLabel() const;
+	ModelComponent* getEnterIntoLabelComponent() const;
+	void sendEntityToLabelComponent(Entity* entity, double timeDelay);
+protected: // must be overriden 
+	virtual bool _loadInstance(PersistenceRecord *fields);
+	virtual void _saveInstance(PersistenceRecord *fields, bool saveDefaultValues);
+protected: // could be overriden 
+	virtual bool _check(std::string* errorMessage);
+	//virtual ParserChangesInformation* _getParserChangesInformation();
+	//virtual void _initBetweenReplications();
+	//virtual void _createInternalAndAttachedData();
+private:
+	std::string _label;
+	ModelComponent* _enteringLabelComponent = nullptr;
+};
 
-extern "C" StaticGetPluginInformation GetPluginInformation() {
-	return &Label::GetPluginInformation;
-}
-#endif
+#endif /* LABEL_H */
 
-ModelDataDefinition* Label::NewInstance(Model* model, std::string name) {
-	return new Label(model, name);
-}
-
-Label::Label(Model* model, std::string name) : ModelDataDefinition(model, Util::TypeOf<Label>(), name) {
-//	_addProperty(new PropertyT<std::string>(Util::TypeOf<Label>(), "Label",
-//			DefineGetter<Label,std::string>(this, &Label::getLabel),
-//			DefineSetter<Label,std::string>(this, &Label::setLabel)));
-
-	SimulationControlGeneric<std::string>* propLabel = new SimulationControlGeneric<std::string>(
-									std::bind(&Label::getLabel, this), std::bind(&Label::setLabel, this, std::placeholders::_1),
-									Util::TypeOf<Label>(), getName(), "Label", "");
-
-	_parentModel->getControls()->insert(propLabel);
-
-	// setting properties
-	_addProperty(propLabel);													
-}
-
-// static
-
-ModelDataDefinition* Label::LoadInstance(Model* model, PersistenceRecord *fields) {
-	Label* newElement = new Label(model);
-	try {
-		newElement->_loadInstance(fields);
-	} catch (const std::exception& e) {
-
-	}
-	return newElement;
-}
-
-PluginInformation* Label::GetPluginInformation() {
-	PluginInformation* info = new PluginInformation(Util::TypeOf<Label>(), &Label::LoadInstance, &Label::NewInstance);
-	std::string help = "Label modeldatum module represents a name to where entities can be sent to.";
-	help += "Any receiving transfer module such as 'Enter' can be assigned to a label.";
-	help += "Any other sending transfer module such as 'Route' can send an entity to a label, which corresponds to send it to the receiving module";
-	help += "TIPICAL USES include sending an entity to somewhere else without a direct connection.";
-	info->setDescriptionHelp(help);
-	//info->setDescriptionHelp("");
-	//info->setObservation("");
-	//info->setMinimumOutputs();
-	//info->setDynamicLibFilenameDependencies();
-	//info->setFields();
-	// ...
-	return info;
-}
-
-//
-
-std::string Label::show() {
-	return ModelDataDefinition::show();
-}
-
-void Label::setLabel(std::string _label) {
-	this->_label = _label;
-}
-
-std::string Label::getLabel() const {
-	return _label;
-}
-
-ModelComponent* Label::getEnterIntoLabelComponent() const {
-	return _enteringLabelComponent;
-}
-
-void Label::sendEntityToLabelComponent(Entity* entity, double timeDelay) {
-	//_parentModel->sendEntityToComponent(entity, _enteringLabelComponent->getConnections()->getFrontConnection(), timeDelay);
-	_parentModel->sendEntityToComponent(entity, _enteringLabelComponent, timeDelay);
-}
-
-// must be overriden
-
-bool Label::_loadInstance(PersistenceRecord *fields) {
-	bool res = ModelDataDefinition::_loadInstance(fields);
-	if (res) {
-		try {
-			this->_label = fields->loadField("label", "");
-			std::string componentName = fields->loadField("enteringComponentName", "");
-			ModelComponent* comp = _parentModel->getComponents()->find(componentName);
-			this->_enteringLabelComponent = comp;
-		} catch (...) {
-		}
-	}
-	return res;
-}
-
-void Label::_saveInstance(PersistenceRecord *fields, bool saveDefaultValues) {
-	ModelDataDefinition::_saveInstance(fields, saveDefaultValues);
-	fields->saveField("label", this->_label, "", saveDefaultValues);
-	if (_enteringLabelComponent != nullptr) {
-		fields->saveField("enteringComponentName", _enteringLabelComponent->getName(), "", saveDefaultValues);
-	}
-}
-
-// could be overriden
-
-bool Label::_check(std::string* errorMessage) {
-	bool resultAll = true;
-	resultAll &= (_enteringLabelComponent != nullptr);
-	if (!resultAll) {
-		*errorMessage += "Entering Label Component was not defined";
-	}
-	return resultAll;
-}
-
-//ParserChangesInformation* Label::_getParserChangesInformation() {}
-
-//void Label::_initBetweenReplications() {}
-
-//void Label::_createInternalAndAttachedData() {}
